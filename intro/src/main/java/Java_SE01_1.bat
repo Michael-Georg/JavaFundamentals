6223fed18430989e7dd24cf01d36e8dcf26f1ba2
Michael-Georg <mmgeorg02@gmail.com>
2016-09-03 00:45:46 +0300

    add .bat for 1 task

---
+++ intro/src/main/java/Java_SE01_1.bat
@@ -0,0 +1,5 @@
+javac -d ../../../../classes javase01\t01\main\Main.java
+
+java -cp ./../../../../classes javase01.t01.main.Main
+
+timeout 5
---
+++ intro/src/main/java/javase01/t01/logic/Logic.java
@@ -0,0 +1,12 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Misha on 02.09.2016.
+ */
+public class Logic {
+    public String method() {
+        return "I am string in Logic.";
+
+    }
+
+}
